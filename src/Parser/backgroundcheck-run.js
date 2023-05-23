9d45312f4c80db8fec9e5102b2f9ad8d5e31d608
Sum1002 <sumaiya14035@gmail.com>
2023-05-23 10:28:36 +0600

    Add backgroundcheck-run

---
+++ src/Parser/backgroundcheck-run.js
@@ -0,0 +1,96 @@
+const fs = require('fs');
+const path = require('path');
+const puppeteer = require('puppeteer-extra');
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+puppeteer.use(StealthPlugin());
+const funcs = require('./functions');
+const logger = require('./other/logger');
+
+let rawdata = fs.readFileSync(path.resolve(__dirname, './config.json'));
+let config = JSON.parse(rawdata);
+let browser, page;
+// let proxyNumber = funcs.randomInt(0, 1);
+// 0 - cheaper proxy, 1 - expensive proxy
+let proxyNumber = 0;
+let firstname = process.argv[2];
+let lastname = process.argv[3];
+let city = process.argv[4];
+let state = process.argv[5];
+
+(async () => {
+    try {
+        browser = await puppeteer.launch({
+            slowMo: 100,
+            headless: true,
+            devtools: true,
+            args: [
+                '--proxy-server=' + config.proxy[proxyNumber].host,
+                '--no-sandbox',
+                '--disable-setuid-sandbox',
+                "--disable-gpu",
+                "--disable-dev-shm-usage"
+            ],
+        });
+
+        page = await browser.newPage()
+        await page.setJavaScriptEnabled(true);
+        await page.setDefaultNavigationTimeout(0);
+        await page.setDefaultTimeout(30000);
+        await page.setRequestInterception(true);
+
+        await page.setViewport({
+            width: 1920 + Math.floor(Math.random() * 100),
+            height: 3000 + Math.floor(Math.random() * 100),
+            deviceScaleFactor: 1,
+            hasTouch: false,
+            isLandscape: false,
+            isMobile: false,
+        });
+
+        page.on('request', (req) => {
+            if (
+                req.resourceType() === 'image'
+                || req.resourceType() === 'stylesheet'
+                || req.resourceType() === 'font'
+                || req.url().includes('amazon')
+                || req.url().includes('youtube')
+                || req.url().includes('google')
+                || req.url().includes('adservice')
+            ) {
+                req.abort();
+            } else {
+                req.continue();
+            }
+        });
+
+        await page.authenticate({
+            username: config.proxy[proxyNumber].user,
+            password: config.proxy[proxyNumber].pass
+        });
+        await page.goto(`https://backgroundcheck.run/ng/profile/search?fname=${firstname}&lname=${lastname}&state=${state}&city=${city}`);
+        await page.waitForSelector('.b-pfl-list');
+
+        const results = await page.evaluate(() => {
+            let res = [];
+            let profileList = Array.from(document.querySelectorAll('.b-pfl-list')).slice(0, 10);
+            profileList.map(profile => {
+                let name, age, location, link;
+                name = (profile.querySelector('.name')?.textContent.trim());
+                age = profile.querySelector('.age')?.textContent;
+                location = Array.from(profile.querySelectorAll('span'))
+                    .find((elem) => elem.textContent?.includes('Has lived in'))
+                    ?.nextElementSibling
+                    ?.textContent;
+                link = profile.href;
+                res.push({name, age, location, link});
+            })
+            return res;
+        });
+        console.log(JSON.stringify({message: results, error: null}));
+    } catch (e) {
+        console.log(JSON.stringify({message: null, error: e.message}));
+        logger.error(JSON.stringify(e, Object.getOwnPropertyNames(e)));
+    } finally {
+        process.exit(0);
+    }
+})();
