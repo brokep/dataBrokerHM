44ae024a99c975d1c91dcc00ba2e5ac026ebbe19
Sum1002 <sumaiya14035@gmail.com>
2023-05-30 08:59:21 +0600

    Add freepeoplesearch

---
+++ src/Parser/test.js
@@ -0,0 +1,75 @@
+const fs = require('fs');
+const path = require('path');
+const puppeteer = require('puppeteer-extra');
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+puppeteer.use(StealthPlugin());
+const funcs = require('./functions');
+const logger = require('./other/logger');
+
+let rawdata = fs.readFileSync(path.resolve(__dirname, './config.json'));
+let config = JSON.parse(rawdata);
+let browser, page;
+// let proxyNumber = funcs.randomInt(0, 1);
+// 0 - cheaper proxy, 1 - expensive proxy
+let proxyNumber = 0;
+let link = process.argv[2];
+
+(async () => {
+    try {
+        browser = await puppeteer.launch({
+            slowMo: 100,
+            headless: false,
+            devtools: true,
+            args: [
+                '--proxy-server=' + config.proxy[proxyNumber].host,
+                '--no-sandbox',
+                '--disable-setuid-sandbox',
+                "--disable-gpu",
+                "--disable-dev-shm-usage"
+            ],
+        });
+
+        page = await browser.newPage()
+        await page.setJavaScriptEnabled(true);
+        await page.setDefaultNavigationTimeout(0);
+        await page.setDefaultTimeout(60000 * 30);
+        await page.setRequestInterception(true);
+
+        await page.setViewport({
+            width: 1920 + Math.floor(Math.random() * 100),
+            height: 3000 + Math.floor(Math.random() * 100),
+            deviceScaleFactor: 1,
+            hasTouch: false,
+            isLandscape: false,
+            isMobile: false,
+        });
+
+        page.on('request', (req) => {
+            if (
+                req.resourceType() === 'image'
+                || req.resourceType() === 'stylesheet'
+                || req.resourceType() === 'font'
+                || req.url().includes('amazon')
+                || req.url().includes('youtube')
+                || req.url().includes('google')
+                || req.url().includes('adservice')
+            ) {
+                req.abort();
+            } else {
+                req.continue();
+            }
+        });
+
+        await page.authenticate({
+            username: config.proxy[proxyNumber].user,
+            password: config.proxy[proxyNumber].pass
+        });
+        await page.goto(link);
+        await page.waitForSelector('#blocker');
+    } catch (e) {
+        console.log(JSON.stringify({message: null, error: e.message}));
+        logger.error(JSON.stringify(e, Object.getOwnPropertyNames(e)));
+    } finally {
+        process.exit(0);
+    }
+})();
